818e6e6aa680892086ebdac321c5586785694e9b
Leo Nguyen <leo.t.beast@gmail.com>
2026-03-14 21:37:27 -0700

    finished homer need to tweak

--- face.js
+++ face.js
@@ -5,13 +5,16 @@ var ydirFlag = 0;
 
 // const el = document.querySelector('.divchange');
 // const rect = el.getBoundingClientRect();
-var origin = [window.innerWidth/2,window.innerHeight/2];
+// var origin = [window.innerWidth/2,window.innerHeight/2];
+
+//replace if not want hardcoded and tweak
+var origin = [723,557];
 var rotateAngle = 0;
 
 document.addEventListener("mousemove", function(event) {
     // console.log(event.clientX,event.clientY)
-    origin = [window.innerWidth/2,window.innerHeight/2];
-    rotateAngle = mathTime(event.clientX,event.clientY)
+    //js find one angle and good enough
+    rotateAngle = mathTime(event.clientX,event.clientY,origin)
     rotateImage(rotateAngle)
     // console.log(rotateAngle)
 });
@@ -19,29 +22,23 @@ document.addEventListener("mousemove", function(event) {
 
 // i firkeing love funcitons okay jeesh louish
 function rotateImage (rotateAngle) {
-    const image = document.getElementById("rotatings");
-    angle = -1*(rotateAngle+90)
-
-    //please math ts out cuh
-    // if (rotateAngle < 0) {
-    //     angle = -1*(rotateAngle-90)
-    // } else {
-    //     angle = -1*(rotateAngle+90)
-    // }
-
-    image.style.transform = `rotate(${angle}deg)`;
+    const images = document.querySelectorAll("#rotatings img");
+    let angle = -1*(rotateAngle+90)
+    images.forEach(image => {
+        //wstackoverflow
+        image.style.transform = `rotate(${angle}deg)`;
+    })
 }
 function findAngle (x,y) {
     //change cuz in radians
     degs = Math.atan2(y,x) * 180 / Math.PI
     console.log(degs)
     return degs
-
     
 }
 
 
-function mathTime (mouseX,mouseY) {
+function mathTime (mouseX,mouseY,origin) {
     
      disX = mouseX - origin[0]
      disY = origin[1] - mouseY 
